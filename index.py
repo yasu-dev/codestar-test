db0109b76fe105fa3153b54a118181bd1a66358f
EC2 Default User <ec2-user@ip-172-31-32-184.ap-northeast-1.compute.internal>
2021-10-01 02:33:29 +0000

    update

--- index.py
+++ index.py
@@ -1,12 +1,31 @@
+import sys
+import logging
+import boto3
 import json
-import datetime
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource('dynamodb')
+table_name = 'Topic'
+table = dynamodb.Table(table_name)
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+
+    logger.info('DELETE topics')
+
+    response = table.delete_item(
+        Key={
+            'topicId': int(event['pathParameters']['topicID'])
+        }
+    )
+
+    return {
+            'isBase64Encoded': False,
+            'statusCode': 200,
+            'headers': {
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps(event['pathParameters']['topicID'])
     }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
