5d8b410ed03500dba55a1e9bcc6ac1ccba1be05b
EC2 Default User <ec2-user@ip-172-31-32-184.ap-northeast-1.compute.internal>
2021-10-01 06:51:08 +0000

    update

--- index.py
+++ index.py
@@ -21,7 +21,6 @@ def handler(event, context):
     )
 
     return {
-            'isBase64Encoded': False,
             'statusCode': 200,
             'headers': {
               'Content-Type': 'application/json',
