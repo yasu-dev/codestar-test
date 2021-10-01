490a71720f412c8a04777b7d0459f75136ade5e3
EC2 Default User <ec2-user@ip-172-31-32-184.ap-northeast-1.compute.internal>
2021-10-01 02:37:11 +0000

    update

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,16 +1,17 @@
 import unittest
 import index
 
-
 class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
+
+        event = {'topicId': 1}
+
         print("testing response.")
-        result = index.handler(None, None)
+        result = index.handler(event, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
-
 
 if __name__ == '__main__':
     unittest.main()
